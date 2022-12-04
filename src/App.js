60ced2b4dee7cc907477acf9b1b2c8c359462517
prasanth <prasanth_varghese>
2022-12-04 22:11:36 +0530

    Assignemnt 6 -render values based on api json repsonse

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import { Component } from 'react';
 
-import logo from './logo.svg';
+
 import './App.css';
 
 class  App extends Component {
@@ -9,32 +9,28 @@ class  App extends Component {
   constructor() {
     super();
     this.state ={
-      names: [
-        { id: 1,
-          value: 'Prasanth'
-        },
-        { id: 2,
-          value: 'Kevin'
-        },
-        { id: 3,
-          value: 'Manu'
-        }
-      ]
+      myusers: [],
     }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>response.json())
+    .then((users)=> this.setState(() => {
+      return {myusers: users};
+    },));
   }
 
    render() {
     return (
       <div className="App">
-        <header className="App-header">
           {
-            this.state.names.map(name=>{
-              return <div id={name.id}>
-                <h1>{name.value}</h1>
+            this.state.myusers.map(eachname=>{
+              return <div id={eachname.id}>
+                <h1>{eachname.name}</h1>
                 </div>
             })
           }
-        </header>
       </div>
     );
   }
